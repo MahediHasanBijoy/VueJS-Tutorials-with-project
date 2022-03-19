ecb5c7851700e88b54b65915a6b82facbcb451e8
MahediHasanBijoy <bijoycse2014@gmail.com>
2022-03-19 10:47:56 +0600

    rendering conditional content completed

--- 03 - Rendering Conditional Content & Lists/lists-cond-01-starting/app.js
+++ 03 - Rendering Conditional Content & Lists/lists-cond-01-starting/app.js
@@ -1,7 +1,28 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      enteredGoal: '',
+      goals: []
+       };
   },
+  methods:{
+    addGoal(){
+      this.goals.push(this.enteredGoal);
+
+    },
+    removeGoal(index){
+      this.goals.splice(index,1);
+    }
+  },
+  computed: {
+    lengthZero(){
+      if(this.goals.length === 0){
+        return true;
+      }else {
+        return false;
+      }
+    }
+  }
 });
 
 app.mount('#user-goals');
